2a5055de3171c9caf18a2a5845a13137e7754bfc
Pierre-Julien Cazaux <cazaux.pierre@gmail.com>
2017-01-07 16:03:59 +0100

    Updated

--- index.js
+++ index.js
@@ -1,4 +1,56 @@
 var SerialPort = require("serialport");
-var port = new SerialPort("/dev/tty-usbserial1", {
-  baudRate: 57600
+//require('tty').setRawMode(true);    
+
+var selector = process.argv[2] || "wchusbserial";
+var portBaudRate = process.argv[3] || 9600;
+
+var selectedPort;
+var matchingPort;
+
+var pinger;
+
+SerialPort.list(function (err, ports) {
+  ports.forEach(function(port) {
+    console.log(port.comName);
+    //console.log(port.pnpId);
+    //console.log(port.manufacturer);
+
+    if(port.comName.includes(selector)) matchingPort = port;;
+  });
+  if(matchingPort){
+    console.log("Starting listening this");
+    for (key in matchingPort) {
+      console.log("\t", key + ": ", matchingPort[key]);
+    };
+    startPort(matchingPort);
+  }
+});
+
+
+var startPort = function(port){
+  var port = new SerialPort(port.comName, {
+    baudRate: portBaudRate,
+    parser: SerialPort.parsers.readline('\n')
+  });
+
+  port.on('data', function (data) {
+    /*if(pinger == undefined) {
+      pinger = setInterval(function(){
+        send(port, "0");
+      }, 5000);
+    }*/
+  console.log(data);
+});
+}
+
+var send = function (port, message) {
+  port.write(message+'\n');
+};
+
+var stdin = process.openStdin(); 
+
+
+stdin.on('keypress', function (chunk, key) {
+  process.stdout.write('Get Chunk: ' + chunk + '\n');
+  if (key && key.ctrl && key.name == 'c') process.exit();
 });
